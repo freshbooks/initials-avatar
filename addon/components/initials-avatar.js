3f9f18bcf386d143ca1b31c2c398edf33d234245
Shaun Kaasten <shaunk@freshbooks.com>
2014-12-17 17:22:59 -0500

    Rather than set styles, set a class name based on color index.

--- addon/components/initials-avatar.js
+++ addon/components/initials-avatar.js
@@ -3,17 +3,20 @@ export default Ember.Component.extend({
   firstName: '',
   lastName: '',
   company: '',
+  colorIndex: 1,
+  maxColorIndex: 1,
   firstInitial: (function() {
     return this.get("firstName")[0] || this.get("company")[0];
   }).property('firstName', 'lastName'),
   lastInitial: (function() {
     return this.get("lastName")[0] || "";
   }).property('lastName'),
-  classNames: 'initials-avatar',
-  attributeBindings: ['style'],
-  style: (function() {
-      var computedColor = (this.get("colorOffset") !== void 0 ? this.get("colors")[this.get("colorOffset") % this.get("colors").length] : "#CCCCCC");
-    return "color: " + computedColor + "; border-color:" + computedColor;
-  }).property('color-offset'),
-  colors: ["#FFCCCC", "#CCFFCC", "#CCCCFF"]
+  classNameBindings: [':initials-avatar', 'avatarColor'],
+  avatarColor: (function() {
+    var index = this.get("colorIndex");
+    if (index > this.get("maxColorIndex")) {
+        index = index % this.get("maxColorIndex");
+    }
+    return 'avatar-color-' + index;
+  }).property('maxColors', 'colorIndex')
 });
