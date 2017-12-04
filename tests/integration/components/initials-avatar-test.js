a6ffa2eae5bd6ef8bb25f14e86fb10f4fcccaad0
Miguel Andrade <jmandrade0@gmail.com>
2017-12-04 04:00:25 +0000

    add support for color property

--- tests/integration/components/initials-avatar-test.js
+++ tests/integration/components/initials-avatar-test.js
@@ -33,3 +33,19 @@ test('displays image if one is provided', function(assert) {
   assert.equal(this.$('#initials-avatar').attr('style'), 'background-image: url(http://example.com/potus.jpg); background-size: cover');
   assert.equal(this.$('#initials-avatar').text().trim(), '');
 });
+
+test('uses color if no image', function(assert) {
+  assert.expect(2);
+
+  this.render(hbs`
+    {{initials-avatar
+      id="initials-avatar"
+      color="#ffffff"
+      firstName="Barack"
+      lastName="Obama"
+    }}
+  `);
+
+  assert.equal(this.$('#initials-avatar').attr('style'), 'background-color: #ffffff;');
+  assert.equal(this.$('#initials-avatar').text().trim(), 'BO');
+});
